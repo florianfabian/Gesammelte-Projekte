ae2a09e39a634811f5d50e5cba307b5d884742a4
florianfabian <103243374+florianfabian@users.noreply.github.com>
2022-08-27 15:59:51 +0200

    Practice

--- python/10_Classes_and_Objects.py
+++ python/10_Classes_and_Objects.py
@@ -1,1 +1,30 @@
 #Copyright @learnpython.org
+#We have a class defined for vehicles. 
+#Create two new vehicles called car1 and car2. 
+#Set car1 to be a red convertible worth $60,000.00 with a name of Fer, 
+#and car2 to be a blue van named Jump worth $10,000.00.
+
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+car1 = Vehicle()
+car1.name = "Fer"
+car1.color = "red"
+car1.kind = "convertible"
+car1.value = 60000.00
+
+car2 = Vehicle()
+car2.name = "Jump"
+car2.color = "blue"
+car2.kind = "van"
+car2.value = 10000.00
+# test code
+print(car1.description())
+print(car2.description())
---
+++ python/11_Dictionaries.py
@@ -0,0 +1,1 @@
+#Copyrigt @learnpython.org
