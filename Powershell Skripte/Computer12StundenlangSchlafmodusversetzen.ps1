6fe326f293dfabb6d7f2051e872310137c3e2e92
florianfabian <103243374+florianfabian@users.noreply.github.com>
2023-05-04 16:39:03 +0200

    Create Computer12StundenlangSchlafmodusversetzen.ps1

---
+++ Powershell Skripte/Computer12StundenlangSchlafmodusversetzen.ps1
@@ -0,0 +1,18 @@
+﻿Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class PowerManager {
+    [DllImport("Powrprof.dll", SetLastError=true)]
+    public static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
+}
+"@
+
+[PowerManager]::SetSuspendState($true, $false, $false)
+
+Start-Sleep -Seconds (12 * 60 * 60)
+
+$rtcWakeupTime = (Get-Date).AddSeconds(1)
+$wakeupTime = $rtcWakeupTime.ToString("HH:mm:ss")
+powershell -Command "Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation' -Name 'RealTimeIsUniversal' -Value 1"
+shutdown.exe /h /t 1 /c "Scheduled wake up" /fw /rtc:$wakeupTime
