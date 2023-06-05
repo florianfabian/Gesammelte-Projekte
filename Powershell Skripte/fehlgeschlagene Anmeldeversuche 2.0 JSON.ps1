2c41ff8d3fa6b63942583b1d8a6f6ff475e99ce4
florianfabian <103243374+florianfabian@users.noreply.github.com>
2023-06-05 12:08:49 +0200

    Create fehlgeschlagene Anmeldeversuche 2.0 JSON.ps1

---
+++ Powershell Skripte/fehlgeschlagene Anmeldeversuche 2.0 JSON.ps1
@@ -0,0 +1,22 @@
+﻿# Filter-Definition: Event-ID 4625 steht für fehlgeschlagene Anmeldeversuche
+$filter = @{
+    LogName = 'Security'
+    Id = 4625
+    StartTime = (Get-Date).AddHours(-1)
+}
+
+# Abrufen der Ereignisse basierend auf dem Filter
+$events = Get-WinEvent -FilterHashtable $filter
+
+# Erstellen eines Hash-Objekts für die JSON-Ausgabe
+$output = @{
+    prtg = @{
+        result = @{
+            channel = "Failed Logins"
+            value = $events.Count
+        }
+    }
+}
+
+# Konvertieren des Ausgabe-Objekts in JSON
+$output | ConvertTo-Json
