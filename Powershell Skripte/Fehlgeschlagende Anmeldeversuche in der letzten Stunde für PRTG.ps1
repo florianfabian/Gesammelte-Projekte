b6b0214bfc3cbec69d9460c8ef56265d1ea4b173
florianfabian <103243374+florianfabian@users.noreply.github.com>
2023-06-05 10:44:27 +0200

    update new scripts

---
+++ Powershell Skripte/Fehlgeschlagende Anmeldeversuche in der letzten Stunde für PRTG.ps1
@@ -0,0 +1,20 @@
+﻿# Filter-Definition: Event-ID 4625 steht für fehlgeschlagene Anmeldeversuche
+$filter = @{
+    LogName = 'Security'
+    Id = 4625
+    StartTime = (Get-Date).AddHours(-1)
+}
+
+# Abrufen der Ereignisse basierend auf dem Filter
+$events = Get-WinEvent -FilterHashtable $filter
+
+# Überprüfen, ob fehlgeschlagene Anmeldeversuche gefunden wurden
+if ($events) {
+    Write-Output "Fehlgeschlagene Anmeldeversuche erkannt!"
+    foreach ($event in $events) {
+        # Ausgabe der Details des fehlgeschlagenen Anmeldeversuchs
+        Write-Output $event.Message
+    }
+} else {
+    Write-Output "Keine fehlgeschlagenen Anmeldeversuche in der letzten Stunde gefunden."
+}
---
+++ Powershell Skripte/Fehlgeschlagene Anmeldeversuche für Monitoring Software.ps1
@@ -0,0 +1,19 @@
+﻿# Filter-Definition: Event-ID 4625 steht für fehlgeschlagene Anmeldeversuche
+$filter = @{
+    LogName = 'Security'
+    Id = 4625
+}
+
+# Abrufen der Ereignisse basierend auf dem Filter
+$events = Get-WinEvent -FilterHashtable $filter -MaxEvents 5
+
+# Überprüfen, ob fehlgeschlagene Anmeldeversuche gefunden wurden
+if ($events) {
+    Write-Output "Fehlgeschlagene Anmeldeversuche erkannt!"
+    foreach ($event in $events) {
+        # Ausgabe der Details des fehlgeschlagenen Anmeldeversuchs
+        Write-Output $event.Message
+    }
+} else {
+    Write-Output "Keine fehlgeschlagenen Anmeldeversuche gefunden."
+}
