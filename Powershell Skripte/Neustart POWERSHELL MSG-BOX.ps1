fd82089ec244988e7b3d9a61fb572ac0c07f97fa
florianfabian <103243374+florianfabian@users.noreply.github.com>
2023-06-06 11:51:37 +0200

    Neue Skripte für Neustart

---
+++ Powershell Skripte/Neustart POWERSHELL MSG-BOX.ps1
@@ -0,0 +1,26 @@
+﻿# Letzten Startzeitpunkt des Systems holen
+$os = Get-WmiObject -Class Win32_OperatingSystem
+$lastBootTime = $os.ConvertToDateTime($os.LastBootUpTime)
+
+# Betriebszeit berechnen
+$uptime = New-TimeSpan -Start $lastBootTime -End (Get-Date)
+
+# Nachricht erstellen
+$message = @"
+Es ist nun $($uptime.Days) Tage, $($uptime.Hours) Stunden und $($uptime.Minutes) Minuten her, seit Ihr Computer das letzte Mal neu gestartet wurde.
+
+Regelmäßige Neustarts sind wichtig für die Sicherheit und Leistung Ihres Computers. Wenn Sie Fragen haben, wenden Sie sich bitte an die IT-Abteilung.
+
+Möchten Sie jetzt neu starten?
+"@
+
+# Lade die benötigte .NET Assembly für MessageBox
+Add-Type -AssemblyName System.Windows.Forms
+
+# Nachricht in einer MessageBox anzeigen und Antwort speichern
+$result = [System.Windows.Forms.MessageBox]::Show($message, "Neustart erforderlich", [System.Windows.Forms.MessageBoxButtons]::YesNo)
+
+# Überprüfe das Ergebnis und starte neu, wenn 'Yes' ausgewählt wurde
+if ($result -eq [System.Windows.Forms.DialogResult]::Yes) {
+    Restart-Computer -Confirm
+}
---
+++ Powershell Skripte/Neustart POWERSHELL in der Konsole.ps1
@@ -0,0 +1,24 @@
+﻿# Letzten Startzeitpunkt des Systems holen
+$os = Get-WmiObject -Class Win32_OperatingSystem
+$lastBootTime = $os.ConvertToDateTime($os.LastBootUpTime)
+
+# Betriebszeit berechnen
+$uptime = New-TimeSpan -Start $lastBootTime -End (Get-Date)
+
+# Nachricht erstellen
+$message = @"
+Es ist nun $($uptime.Days) Tage, $($uptime.Hours) Stunden und $($uptime.Minutes) Minuten her, seit Ihr Computer das letzte Mal neu gestartet wurde.
+
+Regelmäßige Neustarts sind wichtig für die Sicherheit und Leistung Ihres Computers. Wenn Sie Fragen haben, wenden Sie sich bitte an die IT-Abteilung.
+
+Möchten Sie jetzt neu starten? (j/n)
+"@
+
+# Nachricht anzeigen und Benutzereingabe abfragen
+$restart = Read-Host -Prompt $message
+
+# Wenn die Benutzereingabe 'j' ist, Computer neu starten
+if ($restart -eq 'j') {
+    Restart-Computer -Confirm
+}
+
