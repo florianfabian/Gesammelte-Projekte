1feffc55dbb4ea9d0db0f740461936c85a0a327a
florianfabian <103243374+florianfabian@users.noreply.github.com>
2023-06-15 16:00:09 +0200

    Create Dell Command Update ausführen ohne Adminrechte.ps1

---
+++ Powershell Skripte/Dell Command Update ausführen ohne Adminrechte.ps1
@@ -0,0 +1,14 @@
+﻿# Pfad zum Dell Command Update-Programm
+$dellCommandUpdatePath = "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe"
+
+# Erstelle ein neues ScheduledTaskAction-Objekt für das Update
+$aktion = New-ScheduledTaskAction -Execute $dellCommandUpdatePath -Argument "/ApplyUpdates"
+
+# Erstelle ein neues ScheduledTaskPrincipal-Objekt, um die Aufgabe mit höchsten Berechtigungen auszuführen
+$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+# Erstelle und registriere die Aufgabe
+Register-ScheduledTask -Action $aktion -Principal $principal -TaskName "DellCommandUpdate" -Description "Führt das Dell Command Update als der aktuell angemeldete Benutzer aus"
+
+# Starte die Aufgabe
+Start-ScheduledTask -TaskName "DellCommandUpdate"
