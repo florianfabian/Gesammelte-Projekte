94dd99c16fbf439036acc14e9eee131c783e46f9
florianfabian <103243374+florianfabian@users.noreply.github.com>
2023-06-15 15:24:45 +0200

    Dell Updates hoffentlich final

---
+++ Powershell Skripte/Dell Updates Neustart nach 15 Minuten.ps1
@@ -0,0 +1,26 @@
+﻿# Lade die benötigte .NET Assembly für MessageBox
+Add-Type -AssemblyName System.Windows.Forms
+
+# Warte 15 Minuten, um die Updates zu ziehen
+Start-Sleep -Seconds 900
+
+# Nachricht für die MessageBox erstellen
+$message = @"
+Updates wurden installiert, und ein Neustart ist zwingend erforderlich.
+Bitte speichern Sie alle offenen Arbeiten.
+Möchten Sie jetzt neu starten? Wenn Sie auf 'Verschieben' klicken, wird der PC in 15 Minuten automatisch neu gestartet.
+"@
+
+# Nachricht in einer MessageBox anzeigen und Antwort speichern
+$result = [System.Windows.Forms.MessageBox]::Show($message, "Neustart erforderlich", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Information, [System.Windows.Forms.MessageBoxDefaultButton]::Button1, [System.Windows.Forms.MessageBoxOptions]::DefaultDesktopOnly)
+
+# Überprüfe das Ergebnis
+if ($result -eq [System.Windows.Forms.DialogResult]::Yes) {
+    # Neustart sofort
+    Restart-Computer -Force
+} else {
+    # Verschiebe den Neustart um 15 Minuten
+    Start-Sleep -Seconds 900
+    # Neustart durchführen
+    Restart-Computer -Force
+}
---
+++ Powershell Skripte/Dell Updates anstoßen EINFACH.ps1
@@ -0,0 +1,11 @@
+﻿# Pfad zum Dell Command Update-Programm
+$dellCommandUpdatePath = "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe"
+
+# Überprüfen, ob das Dell Command Update-Programm existiert
+if (Test-Path $dellCommandUpdatePath) {
+    # Führe das Dell Command Update-Programm aus
+    & $dellCommandUpdatePath /ApplyUpdates
+    Write-Host "Dell Command Update wurde ausgeführt" -ForegroundColor Green
+} else {
+    Write-Host "Dell Command Update ist nicht unter dem Pfad installiert" -ForegroundColor Red
+}
