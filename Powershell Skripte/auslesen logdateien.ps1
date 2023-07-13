0957c5d39025b58450de8854954b7ed71532e829
florianfabian <103243374+florianfabian@users.noreply.github.com>
2023-07-13 12:50:24 +0200

    Create auslesen logdateien.ps1

---
+++ Powershell Skripte/auslesen logdateien.ps1
@@ -0,0 +1,11 @@
+﻿# Der Pfad zur Log-Datei
+$logFile = "Pfad-zur-Log-Datei"
+
+# Laden Sie die Log-Datei
+$logContent = Get-Content $logFile
+
+# Zählen Sie die Anzahl der Log-Einträge
+$numberOfLogEntries = ($logContent | Measure-Object -Line).Lines
+
+# Geben Sie die Anzahl der Log-Einträge für PRTG aus
+write-Host "$($numberOfLogEntries):Log Entries"
